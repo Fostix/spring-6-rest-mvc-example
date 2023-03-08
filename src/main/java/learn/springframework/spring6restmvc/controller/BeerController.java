41a30d3d53afa1d88a64f6e6e164be6d6e76e991
Albert <accretiondiskthat@gmail.com>
2023-03-08 17:09:56 +0500

    Auto generate constructor lombok

---
+++ src/main/java/learn/springframework/spring6restmvc/controller/BeerController.java
@@ -0,0 +1,12 @@
+package learn.springframework.spring6restmvc.controller;
+
+import learn.springframework.spring6restmvc.services.BeerService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@AllArgsConstructor
+@Controller
+public class BeerController {
+    private final BeerService beerService;
+
+}
