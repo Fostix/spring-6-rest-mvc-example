e7bd7b95dab50d6bb7bf94bf6f1ed097ad569664
Albert <accretiondiskthat@gmail.com>
2023-03-12 21:48:10 +0500

    Database Constraint Validation

--- src/test/java/learn/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
+++ src/test/java/learn/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
@@ -1,5 +1,6 @@
 package learn.springframework.spring6restmvc.repositories;
 
+import jakarta.validation.ConstraintViolationException;
 import learn.springframework.spring6restmvc.entities.Beer;
 import learn.springframework.spring6restmvc.model.BeerStyle;
 import org.junit.jupiter.api.Test;
@@ -15,6 +16,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 class BeerRepositoryTest {
     @Autowired
     BeerRepository beerRepository;
+
+    @Test
+    void testSaveBeerNewTooLong() {
+        assertThrows(ConstraintViolationException.class, () -> {
+            Beer savedBeer = beerRepository.save(Beer.builder()
+                    .beerName("My Beer 5720943587723098757209435877230987572094358772309875720943587723098757209435877230987572094358772309875720943587723098757209435877230987")
+                    .beerStyle(BeerStyle.PALE_ALE)
+                    .upc("6230948753240")
+                    .price(new BigDecimal(11.19))
+                    .build());
+
+            beerRepository.flush();
+        });
+    }
 
     @Test
     void testSaveBeer() {
