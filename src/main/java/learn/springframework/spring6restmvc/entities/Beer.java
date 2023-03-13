ef14d0dc1b9e33f9bfedf438749c58fa64956706
Albert <accretiondiskthat@gmail.com>
2023-03-13 16:42:40 +0500

    JPA Updates for MySQL

--- src/main/java/learn/springframework/spring6restmvc/entities/Beer.java
+++ src/main/java/learn/springframework/spring6restmvc/entities/Beer.java
@@ -7,6 +7,8 @@ import jakarta.validation.constraints.Size;
 import learn.springframework.spring6restmvc.model.BeerStyle;
 import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -22,6 +24,7 @@ public class Beer {
     @Id
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @JdbcTypeCode(SqlTypes.CHAR)
     @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
     private UUID id;
     @Version
